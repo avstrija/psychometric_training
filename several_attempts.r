5dd2bcd3da743885f52a137c635b93b326ef8a56
Daria Borisenko-Orlowski <sonja.rosenfeld@gmail.com>
2025-03-07 19:52:01 -0500

    adding specific attempts

--- several_attempts.r
+++ several_attempts.r
@@ -1,11 +1,12 @@
 df <- read.table("book_dataset_2a.tsv", header= TRUE, quote= "", sep= "\t")
-head(as.data.frame(df))
 library(dplyr)
 
 df <- df %>%
     group_by(student_id, task_id) %>%
     arrange(timestamp) %>%
     mutate(attempt = row_number())
+
+head(as.data.frame(df))
 
 p_i <- df %>%
     filter(attempt == 1) %>%
@@ -21,3 +22,22 @@ p_i <- df %>%
 head(as.data.frame(p_i))
 
 p_i[p_i$task_id %in% c("43f464b92996aa28", "5f596b6d2bf8eb9b", "67e3272b9bf1e589", "92fecefdabe81f5c"),]
+
+p_ia <- df %>%
+    filter(attempt <= 3) %>%
+    summarise(
+        n_Correct = sum(is_correct == 1),
+        n_Incorrect = sum(is_correct == 0),
+        .groups = "drop"
+    ) %>%
+    group_by(task_id) %>%
+    mutate(
+        cum_n_Correct = cumsum(n_Correct)
+    ) %>%
+    ungroup() %>%
+    mutate(
+        p_ia = round(cum_n_Correct / (cum_n_Correct + n_Incorrect), 2)
+    ) %>%
+    select(task_id, p_ia)
+
+head(as.data.frame(p_ia))
